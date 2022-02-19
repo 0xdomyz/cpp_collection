6904a4b72c2f97139530ce462a06b165f5dab7f9
0xdomyz <yzdominic@outlook.com>
2022-02-20 00:53:08 +1100

    add code

--- basic.cpp
+++ basic.cpp
@@ -3,6 +3,13 @@
 
 using namespace std;
 
+int func(int var1){
+    if (var1 >= 1){
+        var1++;
+    };
+    return var1;
+};
+
 int main()
 {
 
@@ -11,6 +18,20 @@ int main()
     int nbr;
     nbr = 5;
     double nbr2 = 2.3;
+    int var1, var2;
+    int var[] = {1,2,3};
+
+    if(var2 >= var1 && true){
+        var2 = 3;
+    }else if (var2 ==  3){
+        var1 = 1;
+    };
+    string s1;
+
+    var[2];
+
+    cout << "input:";
+    cin >> s1;
 
     cout << "Hello " << charName << "!" << endl;
     cout << charName.find("def",0) << charName.length() << charName.substr(0,3) << endl;
