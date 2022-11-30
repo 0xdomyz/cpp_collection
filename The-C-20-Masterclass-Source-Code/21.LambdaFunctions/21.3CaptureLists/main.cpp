40e1111f13168072d1a6987559c0481faf222eb8
0xdomyz <yzdominic@outlook.com>
2022-11-30 23:49:43 +1100

    try run codes

--- The-C-20-Masterclass-Source-Code/21.LambdaFunctions/21.3CaptureLists/main.cpp
+++ The-C-20-Masterclass-Source-Code/21.LambdaFunctions/21.3CaptureLists/main.cpp
@@ -1,26 +1,27 @@
 #include <iostream>
 
-int main(){
+int main()
+{
 
-    //Capture lists
+    // Capture lists
     /*
      double a{10};
      double b{20};
-     
+
      auto func = [a,b](){
          std::cout  << "a + b : " << a + b << std::endl;
      };
      func();
      */
 
-    //Capturing by value
+    // Capturing by value
     /*
      int c{42};
-     
+
      auto func = [c](){
          std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
      };
-     
+
      for(size_t i{} ; i < 5 ;++i){
          std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
          func();
@@ -29,18 +30,20 @@ int main(){
 
      */
 
-    //Capture by reference
-     int c{42};
-     
-     auto func = [&c](){
-         std::cout << "Inner value : " << c << " &inner : " <<&c <<  std::endl;
-     };
-     
-     for(size_t i{} ; i < 5 ;++i){
-         std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
-         func();
-         ++c;
-     }
-    
+    // Capture by reference
+    int c{42};
+
+    auto func = [&c]()
+    {
+        std::cout << "Inner value : " << c << " &inner : " << &c << std::endl;
+    };
+
+    for (size_t i{}; i < 5; ++i)
+    {
+        std::cout << "Outer value : " << c << " &outer : " << &c << std::endl;
+        func();
+        ++c;
+    }
+
     return 0;
 }
--- The-C-20-Masterclass-Source-Code/24.FunctionTemplates/24.4TemplateTypeParametersByReference/main.cpp
+++ The-C-20-Masterclass-Source-Code/24.FunctionTemplates/24.4TemplateTypeParametersByReference/main.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
 
-template <typename T> const T& maximum(const T& a, const T& b); // Declaration
-//template <typename T> T maximum(T a, T b); // Declaration
+template <typename T>
+const T &maximum(const T &a, const T &b); // Declaration
+// template <typename T> T maximum(T a, T b); // Declaration
 
-int main(){
+int main()
+{
 
-    double a {23.5};
-    double b {51.2};
+    double a{23.5};
+    double b{51.2};
 
     std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
-    auto result = maximum(a,b);
+    auto result = maximum(a, b);
     std::cout << "Out - &a: " << &a << std::endl; // 0x111abc
-  
+
     return 0;
 }
 
-
-//Definition
-template <typename T> const T& maximum(const T& a, const T& b){
+// Definition
+template <typename T>
+const T &maximum(const T &a, const T &b)
+{
     std::cout << "In - &a: " << &a << std::endl; // // 0x111abc
-    return (a > b ) ? a : b ;
+    return (a > b) ? a : b;
 }
 
 /*
