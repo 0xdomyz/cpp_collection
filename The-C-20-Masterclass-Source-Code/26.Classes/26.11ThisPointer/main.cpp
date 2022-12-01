84bb39d1c24dfaa923180ef2fc111d10793c9e7a
0xdomyz <yzdominic@outlook.com>
2022-12-01 22:36:39 +1100

    wotk thru

--- The-C-20-Masterclass-Source-Code/26.Classes/26.11ThisPointer/main.cpp
+++ The-C-20-Masterclass-Source-Code/26.Classes/26.11ThisPointer/main.cpp
@@ -1,58 +1,63 @@
 #include <iostream>
 
+class Dog
+{
+public:
+    Dog() = default;
+    Dog(std::string_view name_param, std::string_view breed_param, int age_param);
+    ~Dog();
 
-class Dog{
-    public : 
-        Dog() = default;
-        Dog(std::string_view name_param, std::string_view breed_param, int  age_param);
-        ~Dog();
+    void print_info()
+    {
+        std::cout << "Dog (" << this << ") : [ name : " << name
+                  << " breed : " << breed << " age : " << *p_age << "]" << std::endl;
+    }
 
-        void print_info(){
-            std::cout << "Dog (" << this << ") : [ name : " << name 
-                << " breed : " << breed << " age : " << *p_age << "]" << std::endl;
-        }
+    // Setters
+    // Chained calls using pointers
+    /*
+    Dog* set_dog_name(std::string_view name){
+        //name = name; // This does nothing
+        this->name = name;
+        return this;
+    }
+    Dog* set_dog_breed(std::string_view breed){
+        this->breed = breed;
+        return this;
+    }
 
-        //Setters
-        //Chained calls using pointers
-        /*
-        Dog* set_dog_name(std::string_view name){
-            //name = name; // This does nothing
-            this->name = name;
-            return this;
-        }
-        Dog* set_dog_breed(std::string_view breed){
-            this->breed = breed;
-            return this;
-        }
+    Dog* set_dog_age(int age){
+        *(this->p_age) = age;
+        return this;
+    }
+    */
 
-        Dog* set_dog_age(int age){
-            *(this->p_age) = age;
-            return this;
-        }
-        */
+    // Chained calls using references
+    Dog &set_dog_name(std::string_view name)
+    {
+        // name = name; // This does nothing
+        this->name = name;
+        return *this;
+    }
+    Dog &set_dog_breed(std::string_view breed)
+    {
+        this->breed = breed;
+        return *this;
+    }
 
-       //Chained calls using references
-        Dog& set_dog_name(std::string_view name){
-            //name = name; // This does nothing
-            this->name = name;
-            return *this;
-        }
-        Dog& set_dog_breed(std::string_view breed){
-            this->breed = breed;
-            return *this;
-        }
+    Dog &set_dog_age(int age)
+    {
+        *(this->p_age) = age;
+        return *this;
+    }
 
-        Dog& set_dog_age(int age){
-            *(this->p_age) = age;
-            return *this;
-        }
-
-    private : 
-        std::string name;
-        std::string breed;
-        int * p_age{nullptr};
+private:
+    std::string name;
+    std::string breed;
+    int *p_age{nullptr};
 };
-Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_param){
+Dog::Dog(std::string_view name_param, std::string_view breed_param, int age_param)
+{
     name = name_param;
     breed = breed_param;
     p_age = new int;
@@ -60,14 +65,16 @@ Dog::Dog(std::string_view name_param, std::string_view breed_param, int  age_par
     std::cout << "Dog constructor called for " << name << " at " << this << std::endl;
 }
 
-Dog::~Dog(){
+Dog::~Dog()
+{
     delete p_age;
-    std::cout << "Dog destructor called for " << name << " at " << this <<  std::endl;
+    std::cout << "Dog destructor called for " << name << " at " << this << std::endl;
 }
 
-int main(){
+int main()
+{
 
-    Dog dog1("Fluffy","Shepherd",2); //Constructor
+    Dog dog1("Fluffy", "Shepherd", 2); // Constructor
     dog1.print_info();
 
     /*
@@ -76,17 +83,15 @@ int main(){
     dog1.set_dog_age(4);
     */
 
-    //Chained calls using pointers
-    //dog1.set_dog_name("Pumba")->set_dog_breed("Wire Fox Terrier")->set_dog_age(4);
+    // Chained calls using pointers
+    // dog1.set_dog_name("Pumba")->set_dog_breed("Wire Fox Terrier")->set_dog_age(4);
 
-    //Chained calls using references
+    // Chained calls using references
     dog1.set_dog_name("Pumba").set_dog_breed("Wire Fox Terrier").set_dog_age(4);
 
-
     dog1.print_info();
-   
 
     std::cout << "Done!" << std::endl;
-   //Destructor
+    // Destructor
     return 0;
 }
--- The-C-20-Masterclass-Source-Code/26.Classes/26.12Struct/main.cpp
+++ The-C-20-Masterclass-Source-Code/26.Classes/26.12Struct/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 
-class Dog{
-    public : 
-        std::string m_name;
+class Dog
+{
+public:
+    std::string m_name;
 };
 
 struct Cat
 {
-    public : 
-        std::string m_name;
+public:
+    std::string m_name;
 };
 
-
-struct Point{
+struct Point
+{
     double x;
     double y;
 };
 
-void print_point(const Point& point){
+void print_point(const Point &point)
+{
     std::cout << "Point [ x: " << point.x << ", y : " << point.y << "]" << std::endl;
 }
 
-
-int main(){
+int main()
+{
     Dog dog1;
     Cat cat1;
 
@@ -30,7 +32,6 @@ int main(){
     cat1.m_name = "Juny";
     std::cout << dog1.m_name << std::endl;
     std::cout << cat1.m_name << std::endl;
-
 
     Point point1;
     point1.x = 10;
@@ -42,8 +43,5 @@ int main(){
     point1.y = 2.7;
     print_point(point1);
 
-    
-
-   
     return 0;
 }
