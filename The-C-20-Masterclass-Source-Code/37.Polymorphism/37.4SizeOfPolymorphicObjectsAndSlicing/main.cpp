9557b500e0e4bf1e3beeb26abbeb07e79242bb82
0xdomyz <yzdominic@outlook.com>
2022-12-03 23:34:02 +1100

    work thru

--- The-C-20-Masterclass-Source-Code/37.Polymorphism/37.4SizeOfPolymorphicObjectsAndSlicing/main.cpp
+++ The-C-20-Masterclass-Source-Code/37.Polymorphism/37.4SizeOfPolymorphicObjectsAndSlicing/main.cpp
@@ -3,18 +3,18 @@
 #include "oval.h"
 #include "circle.h"
 
-int main(){
+int main()
+{
 
-     //Comparing object sizes
-     std::cout << "sizeof(Shape) : " << sizeof(Shape) << std::endl; // dynamic : 40
-     std::cout << "sizeof(Oval) : " << sizeof(Oval) << std::endl; // dynamic : 56
-     std::cout << "sizeof(Circle) : " << sizeof(Circle) << std::endl; // dynamic : 56
+    // Comparing object sizes
+    std::cout << "sizeof(Shape) : " << sizeof(Shape) << std::endl;   // dynamic : 40
+    std::cout << "sizeof(Oval) : " << sizeof(Oval) << std::endl;     // dynamic : 56
+    std::cout << "sizeof(Circle) : " << sizeof(Circle) << std::endl; // dynamic : 56
 
-
-     //Slicing
-    Circle circle1(3.3,"Circle1");
+    // Slicing
+    Circle circle1(3.3, "Circle1");
     Shape shape = circle1;
-    shape.draw(); //Shape::draw
+    shape.draw(); // Shape::draw
 
     return 0;
 }
