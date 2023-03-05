451f20f6f26774f35cbf4e158fe4a1219964f103
0xdomyz <yzdominic@outlook.com>
2023-03-05 19:16:01 +1100

    udpate linked list

--- the_c_book/structures/structures/linked_list.c
+++ the_c_book/structures/structures/linked_list.c
@@ -1,3 +1,15 @@
+// gcc linked_list.c -o linked_list && ./linked_list
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct list_ele
+{
+    int data;
+    struct list_ele *pointer;
+};
+
 struct list_ele *
 sortfun(struct list_ele *list)
 {
@@ -40,4 +52,49 @@ sortfun(struct list_ele *list)
     } while (exchange);
 
     return (dummy.pointer);
+}
+
+int main(void)
+{
+    struct list_ele *list = NULL;
+    struct list_ele *p = NULL;
+    struct list_ele *q = NULL;
+    int i = 0;
+
+    // seed the random number generator
+    srand(time(NULL));
+
+    for (i = 0; i < 10; i++)
+    {
+        p = (struct list_ele *)malloc(sizeof(struct list_ele));
+
+        // p->data = i;
+
+        // assign random numbers
+        p->data = rand() % 100; // 0 - 99
+
+        p->pointer = list;
+        list = p;
+    }
+
+    // print the list
+    printf("The list is:\n");
+    p = list;
+    while (p)
+    {
+        printf("%d\n", p->data);
+        p = p->pointer;
+    }
+
+    q = sortfun(list);
+
+    // print the list
+    printf("The list is now:\n");
+    while (q)
+    {
+        printf("%d\n", q->data);
+        q = q->pointer;
+    }
+
+    return 0;
 }
