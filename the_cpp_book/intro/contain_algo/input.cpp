c5c63d408cb4db73dd1c43527a90019d3d2556c8
0xdomyz <yzdominic@outlook.com>
2023-03-14 11:35:29 +1100

    udpate iput

--- the_cpp_book/intro/contain_algo/input.cpp
+++ the_cpp_book/intro/contain_algo/input.cpp
@@ -1,1 +1,37 @@
 // Compile and run: g++ input.cpp -o input && ./input
+
+#include <iostream>
+
+using namespace std;
+
+void f()
+{
+    int i;
+    cin >> i; // read an integer into i
+    double d;
+    cin >> d; // read a double-precision floating-point number into d
+}
+
+void hello()
+{
+    cout << "Please enter your name\n";
+    string str;
+    cin >> str;
+    cout << "Hello, " << str << "!\n";
+}
+
+void hello_line()
+{
+    cout << "Please enter your name\n";
+    string str;
+    getline(cin, str);
+    cout << "Hello, " << str << "!\n";
+}
+
+int main(void)
+{
+    f();
+    hello();
+    cout << "------------------" << endl;
+    hello_line();
+}
