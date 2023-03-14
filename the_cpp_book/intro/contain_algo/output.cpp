3f8faecc625c8593d42755d6d6c61a4cec956655
0xdomyz <yzdominic@outlook.com>
2023-03-14 11:31:23 +1100

    udpate output

--- the_cpp_book/intro/contain_algo/output.cpp
+++ the_cpp_book/intro/contain_algo/output.cpp
@@ -1,1 +1,44 @@
 // Compile and run: g++ output.cpp -o output && ./output
+
+#include <iostream>
+
+using namespace std;
+
+void f()
+{
+    cout << 10;
+}
+
+void g()
+{
+    int i{10};
+    cout << i;
+}
+
+void h(int i)
+{
+    cout << "the value of i is ";
+    cout << i;
+    cout << '\n';
+}
+
+void h2(int i)
+{
+    cout << "the value of i is " << i << '\n';
+}
+
+void k()
+{
+    int b = 'b'; // note: char implicitly converted to int
+    char c = 'c';
+    cout << 'a' << b << c;
+}
+
+int main(void)
+{
+    f();
+    g();
+    h(10);
+    h2(10);
+    k();
+}
