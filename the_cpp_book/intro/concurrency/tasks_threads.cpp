48ef926b4f0d50c62417ef6909c20beda7eb7d18
0xdomyz <yzdominic@outlook.com>
2023-03-16 20:04:41 +1100

    add tasks thread

--- the_cpp_book/intro/concurrency/tasks_threads.cpp
+++ the_cpp_book/intro/concurrency/tasks_threads.cpp
@@ -1,1 +1,26 @@
-// Compile and run: g++ tasks_threads.cpp -o tasks_threads && ./tasks_threads
+// Compile and run: g++ tasks_threads.cpp -o tasks_threads -lpthread && ./tasks_threads
+
+#include <iostream>
+#include <thread>
+#include <pthread.h>
+
+using namespace std;
+
+void f() { cout << "Hello "; }
+struct F
+{
+    void operator()() { cout << "Parallel World!\n"; }
+};
+void user()
+{
+    thread t1{f};   // f() executes in separate thread
+    thread t2{F()}; // F()() executes in separate thread
+    t1.join();      // wait for t1
+    t2.join();      // wait for t2
+}
+
+int main(void)
+{
+    user();
+    return 0;
+}
