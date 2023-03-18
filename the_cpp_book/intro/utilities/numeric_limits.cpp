817538d1db535ea920d0846ecab4bcbd74164b92
0xdomyz <yzdominic@outlook.com>
2023-03-18 20:23:34 +1100

    add nume lim

--- the_cpp_book/intro/utilities/numeric_limits.cpp
+++ the_cpp_book/intro/utilities/numeric_limits.cpp
@@ -1,1 +1,15 @@
 // Compile and run: g++ numeric_limits.cpp -o numeric_limits && ./numeric_limits
+
+#include <limits>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    static_assert(numeric_limits<char>::is_signed, "unsigned characters!");
+    static_assert(100000 < numeric_limits<int>::max(), "small ints!");
+
+    cout << "max char: " << numeric_limits<char>::max() << '\n';
+    cout << "max int: " << numeric_limits<int>::max() << '\n';
+}
