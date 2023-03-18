1a0325ec72ea0967058029f484c5779e7fc4642e
0xdomyz <yzdominic@outlook.com>
2023-03-18 19:45:18 +1100

    add regexp

--- the_cpp_book/intro/utilities/regexp.cpp
+++ the_cpp_book/intro/utilities/regexp.cpp
@@ -1,1 +1,26 @@
-// Compile and run: g++ regexp.cpp -o regexp && ./regexp
+// Compile and run: g++ regexp.cpp -o regexp -std=c++11 && ./regexp
+// XX12345-4567
+
+#include <regex>
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    regex pat(R"(\w{2}\s*\d{5}(-\d{4})?)");
+    // regex pat(R"(\d)");
+    // ZIP code pattern: XXddddd-dddd and var iants
+
+    int lineno = 0;
+    for (string line; getline(cin, line);)
+    { // read into line buffer
+        ++lineno;
+        smatch matches;                       // matched strings go here
+        if (regex_search(line, matches, pat)) // search for pat in line
+            cout << lineno << ": " << matches[0] << '\n';
+        // if empty, break
+        if (matches.empty())
+            break;
+    }
+}
