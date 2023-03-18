45963430b4f54f1404965ba50b30ded6d9f4e7c7
0xdomyz <yzdominic@outlook.com>
2023-03-18 20:03:46 +1100

    add complex

--- the_cpp_book/intro/utilities/complex.cpp
+++ the_cpp_book/intro/utilities/complex.cpp
@@ -1,1 +1,31 @@
 // Compile and run: g++ complex.cpp -o complex && ./complex
+
+#include <cmath>
+#include <iostream>
+#include <complex>
+
+using namespace std;
+
+// template <typename Scalar>
+// class complex
+// {
+// public:
+//     complex(const Scalar &re = {}, const Scalar &im = {});
+//     // ...
+// };
+
+complex<double> f(complex<double> fl, complex<double> db)
+{
+    complex<long double> ld{fl + sqrt(db)};
+    db += fl * 3.0;
+    fl = pow(1.0 / fl, 2);
+    return fl;
+}
+
+int main(void)
+{
+    complex<double> fl{1.0, 2.0};
+    complex<double> db{3.0, 4.0};
+    complex<double> res = f(fl, db);
+    cout << res << '\n';
+}
