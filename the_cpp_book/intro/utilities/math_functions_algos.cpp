aeacf536da444e129913399f84c68a1d650dbd0e
0xdomyz <yzdominic@outlook.com>
2023-03-18 19:53:38 +1100

    add math func algo

--- the_cpp_book/intro/utilities/math_functions_algos.cpp
+++ the_cpp_book/intro/utilities/math_functions_algos.cpp
@@ -1,1 +1,25 @@
 // Compile and run: g++ math_functions_algos.cpp -o math_functions_algos && ./math_functions_algos
+
+#include <iostream>
+#include <cmath>
+#include <list>
+#include <numeric>
+
+using namespace std;
+
+void f()
+{
+    list<double> lst{1, 2, 3, 4, 5, 9999.99999};
+    auto s = accumulate(lst.begin(), lst.end(), 0.0); // calculate the sum
+    // 4 decimla places, not scientific notation
+    cout.setf(ios_base::fixed, ios_base::floatfield);
+    cout.precision(10);
+    cout << s << '\n'; // print 10014.9999
+    // revert
+    cout.unsetf(ios_base::floatfield);
+}
+
+int main(void)
+{
+    f();
+}
