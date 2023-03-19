fe6443b5b2f1eb339f77a2585e7504201a206f19
0xdomyz <yzdominic@outlook.com>
2023-03-19 16:17:37 +1100

    add special ops

---
+++ the_cpp_book/abstraction/special_ops/subscript.cpp
@@ -0,0 +1,38 @@
+// complie: g++ -o subscript subscript.cpp
+// make a file of 3 words: echo "hello world hello" > words.txt
+// run: ./subscript < words.txt
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+struct Assoc
+{
+    vector<pair<string, int>> vec; // vector of {name,value} pairs
+    const int &operator[](const string &) const;
+    int &operator[](const string &);
+};
+
+int &Assoc::operator[](const string &s)
+// search for s; return a reference to its value if found;
+// otherwise, make a new pair {s,0} and return a reference to its value
+{
+    for (auto &x : vec)
+        if (x.first == s)
+            return x.second; // return reference to value (§31.2.2)
+
+    vec.push_back({s, 0});    // initial value: 0
+    return vec.back().second; // return last element (§31.2.2)
+}
+
+int main() // count the occurrences of each word on input
+{
+    Assoc values;
+    string buf;
+    while (cin >> buf)
+        ++values[buf];
+    for (auto x : values.vec)
+        cout << '{' << x.first << ',' << x.second << "}\n";
+}
