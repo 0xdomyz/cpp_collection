c1248f0424099a82fa1dc22e5a2290790af47e6f
0xdomyz <yzdominic@outlook.com>
2023-03-26 22:11:58 +1100

    Revert "in class func def"
    This reverts commit 455a44a622dbaa0789e1b67ec3ff4476efd768be.

--- the_cpp_book/abstraction/class/in_class_func_def.cpp
+++ the_cpp_book/abstraction/class/in_class_func_def.cpp
@@ -1,19 +1,1 @@
 // Compile and run: g++ in_class_func_def.cpp -o in_class_func_def && ./in_class_func_def
-
-class Date
-{
-public:
-    void add_month(int n) { m += n; } // increment the Date’s m
-    // ...
-private:
-    int d, m, y;
-};
-// inline void Date::add_month(int n) // add n months
-// {
-//     m += n; // increment the Date’s m
-// }
-
-int main(void)
-{
-    return 0;
-}
