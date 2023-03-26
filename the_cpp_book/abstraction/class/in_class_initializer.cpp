d0d3b75929fab5c824c7ba17ed4091ef5f8d3ea7
0xdomyz <yzdominic@outlook.com>
2023-03-26 22:14:19 +1100

    add various class scripts
    Revert "Revert "in class func def""
    
    This reverts commit c1248f0424099a82fa1dc22e5a2290790af47e6f.

--- the_cpp_book/abstraction/class/in_class_initializer.cpp
+++ the_cpp_book/abstraction/class/in_class_initializer.cpp
@@ -1,1 +1,26 @@
 // Compile and run: g++ in_class_initializer.cpp -o in_class_initializer && ./in_class_initializer
+
+class Date
+{
+    int d{1};
+    int m{2};
+    int y{1991};
+
+public:
+    Date(int, int, int); // day, month, year
+    Date(int, int);      // day, month, today’s year
+    Date(int);           // day, today’s month and year
+    Date();              // default Date: today
+    Date(const char *);  // date in string representatio
+};
+
+Date::Date(int dd)
+    : d{dd}
+{
+    // check that the Date is valid
+}
+
+int main(void)
+{
+    return 0;
+}
--- the_cpp_book/abstraction/class/mutability_through_indirection.cpp
+++ the_cpp_book/abstraction/class/mutability_through_indirection.cpp
@@ -1,1 +1,49 @@
 // Compile and run: g++ mutability_through_indirection.cpp -o mutability_through_indirection && ./mutability_through_indirection
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct cache
+{
+    bool valid;
+    string rep;
+};
+
+class Date
+{
+public:
+    // ...
+    Date(int dd, int mm, int yy) : d{dd}, m{mm}, y{yy}, c{new cache{false, ""}} {};
+    string string_rep() const; // string representation
+private:
+    int d, m, y;                      // representation
+    cache *c;                         // initialize in constr uctor
+    void compute_cache_value() const; // fill what cache refers to
+    // ...
+};
+
+string Date::string_rep() const
+{
+    if (!c->valid)
+    {
+        compute_cache_value();
+        c->valid = true;
+    }
+    return c->rep;
+}
+
+void Date::compute_cache_value() const
+{
+    c->rep = to_string(d) + "/" + to_string(m) + "/" + to_string(y);
+}
+
+int main(void)
+{
+    Date d{1, 2, 1991};
+    const Date cd{3, 4, 1991};
+    cout << d.string_rep() << endl;
+    cout << cd.string_rep() << endl;
+    return 0;
+}
