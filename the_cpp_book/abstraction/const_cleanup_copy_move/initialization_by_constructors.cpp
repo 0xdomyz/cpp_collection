c8a32e68d832e926cca5858cf56e1178ffbc55d4
0xdomyz <yzdominic@outlook.com>
2023-03-31 10:54:48 +1100

    update scirpt

--- the_cpp_book/abstraction/const_cleanup_copy_move/initialization_by_constructors.cpp
+++ the_cpp_book/abstraction/const_cleanup_copy_move/initialization_by_constructors.cpp
@@ -33,11 +33,15 @@ vector<string> v12(77);      // 77 elements with the default value ""
 vector<string> v13{"Booh!"}; // one element with the value "Booh!"
 // vector<string> v4("Booh!"); // error : no constructor takes a string argument
 vector<int *> v5{100, 0}; // 100 int*s initialized to nullptr (100 is not an int*)
-vector<int *> v6{0, 0};   // 2 int*s initialized to nullptr
-vector<int *> v7(0, 0);   // empty vector (v7.size()==0)
-vector<int *> v8;         // empty vector (v7.size()==0)
+vector<int *> v51{10, 0};
+vector<int *> v52(10, 0);
+vector<int *> v6{0, 0}; // 2 int*s initialized to nullptr
+vector<int *> v7(0, 0); // empty vector (v7.size()==0)
+vector<int *> v8;       // empty vector (v7.size()==0)
 
 int main(void)
 {
+    for (auto x : v52)
+        cout << x << "\n";
     return 0;
 }
