b5aad7c962ff698626f434f75793dc044f9b62ea
0xdomyz <yzdominic@outlook.com>
2023-03-31 20:30:14 +1100

    udpate copy

--- the_cpp_book/abstraction/const_cleanup_copy_move/copy.cpp
+++ the_cpp_book/abstraction/const_cleanup_copy_move/copy.cpp
@@ -1,6 +1,9 @@
 // Compile and run: g++ copy.cpp -o copy && ./copy
 
 #include <array>
+#include <iostream>
+#include <memory>
+#include <algorithm>
 
 using namespace std;
 
@@ -14,32 +17,52 @@ public:
     int size() const { return dim[0] * dim[1]; }
     Matrix(const Matrix &);            // copy constr uctor
     Matrix &operator=(const Matrix &); // copy assignment
-    Matrix(Matrix &&);                 // move constr uctor
-    Matrix &operator=(Matrix &&);      // move assignment
+    // Matrix(Matrix &&);                 // move constr uctor
+    // Matrix &operator=(Matrix &&);      // move assignment
     ~Matrix() { delete[] elem; }
     // ...
 };
 
 template <class T>
-Matrix<T>::Matrix(const Matrix &m) // copy constr uctor
+Matrix<T>::Matrix(const Matrix<T> &m) // copy constr uctor
     : dim{m.dim},
       elem{new T[m.size()]}
 {
     uninitialized_copy(m.elem, m.elem + m.size(), elem); // copy elements
+    // prototype: uninitialized_copy(b,e,o) where b and e denote input iterators
 }
 
 template <class T>
-// Matrix &Matrix::operator=(const Matrix &m) // copy assignment
-// {
-//     if (dim[0] != m.dim[0] || dim[1] != m.dim[1])
-//         throw runtime_error("bad size in Matrix =");
-//     copy(m.elem, m.elem + m.size(), elem); // copy elements
-// }
-Matrix<T> &Matrix<T>::operator=(const Matrix &m) // copy assignment
+Matrix<T> &Matrix<T>::operator=(const Matrix<T> &m) // copy assignment
 {
-    Matrix tmp{m};    // make a copy
-    swap(tmp, *this); // swap tmp’s representation with *this’s
+    if (dim[0] != m.dim[0] || dim[1] != m.dim[1])
+        throw runtime_error("bad size in Matrix =");
+    copy(m.elem, m.elem + m.size(), elem); // copy elements
+    // prototype: copy(b,e,o) where b and e denote input iterators
     return *this;
 }
 
-int main(void) {}
+// template <class T>
+// Matrix<T> &Matrix<T>::operator=(const Matrix &m) // copy assignment
+// {
+//     Matrix<T> tmp{m}; // make a copy
+//     swap(tmp, *this); // swap tmp’s representation with *this’s
+//                       // prototype: swap(a,b) where a and b are of the same type
+//     return *this;
+// }
+
+int main(void)
+{
+    Matrix<int> m1{2, 3}; // initialize m1
+    Matrix<int> m2{m1};   // copy constr uction
+
+    cout << "m1: " << m1.size() << endl;
+    cout << "m2: " << m2.size() << endl;
+
+    Matrix<int> m3{2, 3}; // initialize m3
+    m3 = m1;              // copy assignment
+
+    cout << "m3: " << m3.size() << endl;
+
+    return 0;
+}
