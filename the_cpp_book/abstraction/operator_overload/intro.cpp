87d05837ce8a4d64f32de9bd4543eb74b1ce206b
0xdomyz <yzdominic@outlook.com>
2023-04-03 19:19:50 +1000

    update intro

--- the_cpp_book/abstraction/operator_overload/intro.cpp
+++ the_cpp_book/abstraction/operator_overload/intro.cpp
@@ -2,28 +2,39 @@
 
 #include <iostream>
 
+using namespace std;
+
 class complex
 { // very simplified complex
     double re, im;
 
 public:
-    complex(double r, double i) : re{r}, im{i} {}
-    complex operator+(complex z)
-    {
-        return complex{re + z.re, im + z.im};
-    };
-    complex &operator+=(complex z)
-    {
-        re += z.re;
-        im += z.im;
-        return *this;
-    };
-    complex operator*(complex z)
-    {
-        return complex{re * z.re - im * z.im, re * z.im + im * z.re};
-    };
+    complex(double r, double i) : re{r}, im{i} {};
+    complex operator+(complex);
+    complex &operator+=(complex);
+    complex operator*(complex);
     double real() const { return re; };
     void real(double d) { re = d; };
+    // printable
+    friend std::ostream &operator<<(std::ostream &os, const complex &z)
+    {
+        return os << '(' << z.re << ',' << z.im << ')';
+    };
+};
+
+complex complex::operator+(complex z)
+{
+    return complex{re + z.re, im + z.im};
+};
+complex &complex::operator+=(complex z)
+{
+    re += z.re;
+    im += z.im;
+    return *this;
+};
+complex complex::operator*(complex z)
+{
+    return complex{re * z.re - im * z.im, re * z.im + im * z.re};
 };
 
 void f()
@@ -31,9 +42,14 @@ void f()
     complex a = complex{1, 3.1};
     complex b{1.2, 2};
     complex c{b};
+
+    cout << a << ' ' << b << ' ' << c << '\n';
+
     a = b + c;
     b = b + c * a;
     c = a * b + complex(1, 2);
+
+    cout << a << ' ' << b << ' ' << c << '\n';
 }
 
 void g(complex a, complex b)
