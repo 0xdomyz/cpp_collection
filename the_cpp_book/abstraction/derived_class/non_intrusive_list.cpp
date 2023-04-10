7c27ee175e689f0bd32bb48a9e473e0370c0f19a
0xdomyz <yzdominic@outlook.com>
2023-04-10 11:45:34 +1000

    udpte non intru list

--- the_cpp_book/abstraction/derived_class/non_intrusive_list.cpp
+++ the_cpp_book/abstraction/derived_class/non_intrusive_list.cpp
@@ -2,120 +2,142 @@
 
 #include <iostream>
 
-template <class T>
-class List
+namespace nil
 {
-public:
-    void insert(T); // insert a value at the head of the allocated list
-    T get();        // get the value at the head of the allocated list
-    List() : allocated(0), free(0), head(0){};
-    ~List()
-    {
-        // ... free all the allocated chunks ...
-        while (allocated)
-        {
-            Chunk *p = allocated;
-            allocated = allocated->next;
-            delete p;
-        }
-    };
-    friend std::ostream &operator<<(std::ostream &os, const List<T> &lst)
-    {
-        // ... print the list ...
-        // each line has chunk::chunk_size elements, use modulo to print the elements
-        os << "List: ";
-        int row_size = Chunk::chunk_size;
-        int i = 0;
-        for (Link *p = lst.head; p; p = p->next)
-        {
-            os << p->val << " ";
-            if (++i % row_size == 0)
-                os << std::endl;
-        }
-        os << std::endl;
-        return os;
-    }
 
-private:
-    struct Link // a Link is a node in the list
+    template <class T>
+    struct Link2
     {
         T val;
-        Link *next;
+        Link2 *next;
     };
-    struct Chunk // a chunk is a list of Links
+
+    template <class T>
+    class List
     {
-        enum
+    public:
+        void insert(T); // insert a value at the head of the allocated list
+        T get();        // get the value at the head of the allocated list
+        List() : allocated(0), free(0), head(0){};
+        ~List()
         {
-            chunk_size = 15
+            // ... free all the allocated chunks ...
+            while (allocated)
+            {
+                Chunk *p = allocated;
+                allocated = allocated->next;
+                delete p;
+            }
         };
-        Link v[chunk_size];
-        Chunk *next;
-    };
-    Chunk *allocated; // allocated list, a list of Chunks that have been allocated
-    Link *free;       // free list, a list of Links that are not in use
-    Link *get_free(); // get a free Link
-    Link *head;       // head of what is in use
-};
-
-// if thrown, give a message
-class Underflow
-{
-
-public:
-    Underflow()
-    {
-        std::cout << "Underflow" << std::endl;
-    }
-};
-
-template <class T>
-void List<T>::insert(T val)
-{
-    Link *lnk = get_free(); // get a free link
-
-    lnk->val = val;
-    // insert at the head of the list
-    lnk->next = head;
-    head = lnk;
-}
-
-template <class T>
-T List<T>::get()
-{
-    if (head == 0)
-        throw Underflow{}; // Underflow is my exception class
-
-    Link *p = head; // get the first element
-    head = p->next; // remove it from the list
-    p->next = free; // put it on the free list
-    free = p;       // return the value
-    return p->val;  // return the value
-}
-
-template <class T>
-typename List<T>::Link *List<T>::get_free()
-// typename List<T>::Link * is the same as List<T>::Link *
-{
-    if (free == 0)
-    {
-        // ... allocate a new chunk and place its Links on the free list ...
-        Chunk *p = new Chunk;
-        p->next = allocated;
-        allocated = p;
-        for (int i = 0; i < Chunk::chunk_size; ++i)
-        // the first iteration has i = 0
+        friend std::ostream &operator<<(std::ostream &os, const List<T> &lst)
         {
-            p->v[i].next = free;
-            free = &p->v[i];
+            // ... print the list ...
+            // each line has chunk::chunk_size elements, use modulo to print the elements
+            os << "List: ";
+            int row_size = Chunk::chunk_size;
+            int i = 0;
+            for (Link *p = lst.head; p; p = p->next)
+            {
+                os << p->val << " ";
+                if (++i % row_size == 0)
+                    os << std::endl;
+            }
+            os << std::endl;
+            return os;
         }
+
+    private:
+        struct Link // a Link is a node in the list
+        {
+            T val;
+            Link *next;
+        };
+        struct Chunk // a chunk is a list of Links
+        {
+            enum
+            {
+                chunk_size = 15
+            };
+            Link v[chunk_size];
+            Chunk *next;
+        };
+        Chunk *allocated; // allocated list, a list of Chunks that have been allocated
+        Link *free;       // free list, a list of Links that are not in use
+        Link *get_free(); // get a free Link
+        Link *head;       // head of what is in use
+    };
+
+    // if thrown, give a message
+    class Underflow
+    {
+
+    public:
+        Underflow()
+        {
+            std::cout << "Underflow" << std::endl;
+        }
+    };
+
+    template <class T>
+    void List<T>::insert(T val)
+    {
+        Link *lnk = get_free(); // get a free link
+
+        lnk->val = val;
+        // insert at the head of the list
+        lnk->next = head;
+        head = lnk;
     }
-    Link *p = free;
-    free = free->next;
-    return p;
+
+    template <class T>
+    T List<T>::get()
+    {
+        if (head == 0)
+            throw Underflow{}; // Underflow is my exception class
+
+        Link *p = head; // get the first element
+        head = p->next; // remove it from the list
+        p->next = free; // put it on the free list
+        free = p;       // return the value
+        return p->val;  // return the value
+    }
+
+    template <class T>
+    typename List<T>::Link *List<T>::get_free()
+    // typename List<T>::Link * is the same as List<T>::Link *
+    {
+        if (free == 0)
+        {
+            // ... allocate a new chunk and place its Links on the free list ...
+            Chunk *p = new Chunk;
+            p->next = allocated;
+            allocated = p;
+            for (int i = 0; i < Chunk::chunk_size; ++i)
+            // the first iteration has i = 0
+            {
+                p->v[i].next = free;
+                free = &p->v[i];
+            }
+        }
+        Link *p = free;
+        free = free->next;
+        return p;
+    }
+
+    template <class T>
+    class Link3
+    {
+        friend class List<T>; // only List<T> can access Link<T>
+        T val;
+        Link3 *next;
+    };
+
 }
 
 int main(void)
 {
+    using namespace nil;
+
     List<int> lst;
 
     for (int i = 0; i < 31; ++i)
