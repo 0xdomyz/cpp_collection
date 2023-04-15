8a8f4764b133013540199e5ae502f32b72b9521b
0xdomyz <yzdominic@outlook.com>
2023-04-15 16:38:37 +1000

    rename script to calc

--- the_cpp_book/basic/expression/calculator.cpp
+++ the_cpp_book/basic/expression/calculator.cpp
@@ -1,10 +1,40 @@
-// g++ parser.cpp -o parser && ./parser
+// g++ calculator.cpp -o calculator && ./calculator
 
 #include <string>
 #include <iostream>
 #include <map>
 
 using namespace std;
+
+// parser
+
+// grammar
+/*
+program:
+    end // end is end-of-input
+    expr_list end
+
+expr_list:
+    expression print // print is newline or semicolon
+    expression print expr_list
+
+expression:
+    expression + term
+    expression − term
+    term
+
+term:
+    term / primary
+    term ∗ primary
+    primary
+
+primary:
+    number // number is a floating-point literal
+    name // name is an identifier
+    name = expression
+    − primar y
+    ( expression )
+*/
 
 enum class Kind : char
 {
@@ -135,6 +165,12 @@ double prim(bool get) // handle primar ies
         return error("primar y expected");
     }
 }
+
+// input func
+
+// symbol table
+
+// driver
 
 int main()
 {
