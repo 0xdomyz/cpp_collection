4dcfa2bc358d1115ac7e8a556338900b858bc8fd
0xdomyz <yzdominic@outlook.com>
2023-04-17 11:06:32 +1000

    udpate dc as cli tool

--- the_cpp_book/basic/expression/dc.cpp
+++ the_cpp_book/basic/expression/dc.cpp
@@ -1,13 +1,9 @@
-// g++ dc.cpp -o dc && ./dc
-
 #include <iostream> // I/O
 #include <string>   // strings
 #include <map>      // map
 #include <cctype>   // isalpha(), etc.
 
 using namespace std;
-
-// parser
 
 // grammar
 /*
@@ -36,6 +32,8 @@ primary:
     - primar y
     ( expression )
 */
+
+// parser
 
 enum class Kind : char
 {
@@ -321,39 +319,40 @@ void calculate()
     }
 }
 
-int main()
-{
-    table["pi"] = 3.1415926535897932385; // inser t predefined names
-    table["e"] = 2.7182818284590452354;
-    calculate();
-    return no_of_errors;
-}
+// interactive driver
+// g++ dc.cpp -o dc && ./dc
 
-// usage
-//
-// dc "rate=1.1934;150/rate;19.75/rate;217/rate"
-//
-// #include <sstream>.
-//
-// cmd args
-// int main(int argc, char *argv[])
+// int main()
 // {
-//     switch (argc)
-//     {
-//     case 1: // read from standard input
-//         break;
-//     case 2: // read from argument string
-//         ts.set_input(new istringstream{argv[1]});
-//         break;
-//     default:
-//         error("too many arguments");
-//         return 1;
-//     }
 //     table["pi"] = 3.1415926535897932385; // inser t predefined names
 //     table["e"] = 2.7182818284590452354;
 //     calculate();
 //     return no_of_errors;
 // }
+
+// cmd driver
+// g++ dc.cpp -o dc && ./dc "rate=1.1934;150/rate;19.75/rate;217/rate"
+
+#include <sstream>
+
+int main(int argc, char *argv[])
+{
+    switch (argc)
+    {
+    case 1: // read from standard input
+        break;
+    case 2: // read from argument string
+        ts.set_input(new istringstream{argv[1]});
+        break;
+    default:
+        error("too many arguments");
+        return 1;
+    }
+    table["pi"] = 3.1415926535897932385; // inser t predefined names
+    table["e"] = 2.7182818284590452354;
+    calculate();
+    return no_of_errors;
+}
 
 // vector<string> arguments(int argc, char∗ argv[])
 // {
