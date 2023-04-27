41a1803105c006ba4fa8f8ad73771e4146cbc292
0xdomyz <yzdominic@outlook.com>
2023-04-27 11:20:04 +1000

    reorg and add section

--- the_cpp_book/abstraction/matrix_design/matrix.cpp
+++ the_cpp_book/abstraction/matrix_design/matrix.cpp
@@ -1,1 +1,37 @@
 #include "matrix.h"
+
+template <typename T, size_t N>
+template <typename... Exts>
+Matrix<T, N>::Matrix(Exts... exts)
+    : desc{exts...},   // copy extents
+      elems(desc.size) // allocate desc.size elements and default initialize them
+{
+}
+
+template <typename T, size_t N>
+Matrix<T, N>::Matrix(Matrix_initializer<T, N> init)
+{
+    Matrix_impl::derive_extents(init, desc.extents); // deduce extents from initializer list (§29.4.4)
+    elems.reserve(desc.size);                        // make room for slices
+    Matrix_impl::insert_flat(init, elems);           // initialize from initializer list (§29.4.4)
+    assert(elems.size() == desc.size);
+}
+
+template <typename T, size_t N>
+template <typename U>
+Matrix<T, N>::Matrix(const Matrix_ref<U, N> &x)
+    : desc{x.desc}, elems{x.begin(), x.end()} // copy desc and elements
+{
+    static_assert(Convertible<U, T>(), "Matrix constructor: incompatible element types");
+}
+
+// copy assignment
+template <typename T, size_t N>
+template <typename U>
+Matrix<T, N> &Matrix<T, N>::operator=(const Matrix_ref<U, N> &x)
+{
+    static_assert(Convertible<U, T>(), "Matrix =: incompatible element types");
+    desc = x.desc;
+    elems.assign(x.begin(), x.end());
+    return *this;
+}
--- the_cpp_book/abstraction/matrix_design/matrix.h
+++ the_cpp_book/abstraction/matrix_design/matrix.h
@@ -1,47 +1,97 @@
 #include <iostream>
 #include <vector>
 
-using namespace std;
-
-namespace matrix
+namespace Matrix_impl
 {
+}
 
-    template <typename T, size_t N>
-    class Matrix
+using namespace std;
+using namespace Matrix_impl;
+
+template <typename T, size_t N>
+class Matrix
+{
+public:
+    static constexpr size_t order = N;
+    using value_type = T;
+    using iterator = typename std::vector<T>::iterator;
+    using const_iterator = typename std::vector<T>::const_iterator;
+
+    Matrix() = default;
+    Matrix(Matrix &&) = default; // move
+    Matrix &operator=(Matrix &&) = default;
+    Matrix(Matrix const &) = default; // copy
+    Matrix &operator=(Matrix const &) = default;
+    ~Matrix() = default;
+
+    template <typename U>
+    Matrix(const Matrix_ref<U, N> &); // construct from Matrix_ref
+    template <typename U>
+    Matrix &operator=(const Matrix_ref<U, N> &); // assign from Matrix_ref
+
+    template <typename... Exts> // specify the extents
+    explicit Matrix(Exts... exts);
+
+    Matrix(Matrix_initializer<T, N>);            // initialize from list
+    Matrix &operator=(Matrix_initializer<T, N>); // assign from list
+
+    template <typename U>
+    Matrix(initializer_list<U>) = delete; // don’t use {} except for elements
+    template <typename U>
+    Matrix &operator=(initializer_list<U>) = delete;
+
+    static constexpr size_t order() { return N; }              // number of dimensions
+    size_t extent(size_t n) const { return desc.extents[n]; }  // #elements in the nth dimension
+    size_t size() const { return elems.size(); }               // total number of elements
+    const Matrix_slice<N> &descriptor() const { return desc; } // the slice defining subscripting
+
+    T *data() { return elems.data(); } // ‘‘flat’’ element access
+    const T *data() const { return elems.data(); }
+
+    // subscripting and slicing
+    template <typename... Args> // m(i,j,k) subscripting with integers
+    Enable_if<Matrix_impl::Requesting_element<Args...>(), T &>
+    operator()(Args... args);
+    template <typename... Args>
+    Enable_if<Matrix_impl::Requesting_element<Args...>(), const T &>
+    operator()(Args... args) const;
+    template <typename... Args> // m(s1,s2,s3) subscripting with slices
+    Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<T, N>>
+    operator()(const Args &...args);
+    template <typename... Args>
+    Enable_if<Matrix_impl::Requesting_slice<Args...>(), Matrix_ref<const T, N>>
+    operator()(const Args &...args) const;
+
+    Matrix_ref<T, N - 1> operator[](size_t i) { return row(i); } // m[i] row access
+    Matrix_ref<const T, N - 1> operator[](size_t i) const { return row(i); }
+
+    Matrix_ref<T, N - 1> row(size_t n); // row access
+    Matrix_ref<const T, N - 1> row(size_t n) const;
+
+    Matrix_ref<T, N - 1> col(size_t n); // column access
+    Matrix_ref<const T, N - 1> col(size_t n) const;
+
+private:
+    Matrix_slice<N> desc; // slice defining extents in the N dimensions
+    vector<T> elems;      // the elements
+};
+
+namespace Matrix_impl
+{
+    struct slice
     {
-    public:
-        static constexpr size_t order = N;
-        using value_type = T;
-        using iterator = typename std::vector<T>::iterator;
-        using const_iterator = typename std::vector<T>::const_iterator;
-        Matrix() = default;
-        Matrix(Matrix &&) = default; // move
-        Matrix &operator=(Matrix &&) = default;
-        Matrix(Matrix const &) = default; // copy
-        Matrix &operator=(Matrix const &) = default;
-        ~Matrix() = default;
-        template <typename U>
-        Matrix(const Matrix_ref<U, N> &); // construct from Matrix_ref
-        template <typename U>
-        Matrix &operator=(const Matrix_ref<U, N> &); // assign from Matrix_ref
-        template <typename... Exts>                  // specify the extents
-        explicit Matrix(Exts... exts);
-        Matrix(Matrix_initializer<T, N>);            // initialize from list
-        Matrix &operator=(Matrix_initializer<T, N>); // assign from list
-        template <typename U>
-        Matrix(initializer_list<U>) = delete; // don’t use {} except for elements
-        template <typename U>
-        Matrix &operator=(initializer_list<U>) = delete;
-        static constexpr size_t order() { return N; }              // number of dimensions
-        size_t extent(size_t n) const { return desc.extents[n]; }  // #elements in the nth dimension
-        size_t siz e() const { return elems.size(); }              // total number of elements
-        const Matrix_slice<N> &descriptor() const { return desc; } // the slice defining subscripting
-        T *data() { return elems.data(); }                         // ‘‘flat’’ element access
-        const T *data() const { return elems.data(); }
-        // ...
-    private:
-        Matrix_slice<N> desc; // slice defining extents in the N dimensions
-        vector<T> elems;      // the elements
+        slice() : start(-1), length(-1), stride(1) {}
+        explicit slice(size_t s) : start(s), length(-1), stride(1) {}
+        slice(size_t s, size_t l, size_t n = 1) : start(s), length(l), stride(n) {}
+        size_t operator()(size_t i) const { return start + i * stride; }
+        static slice all;
+        size_t start;  // first index
+        size_t length; // number of indices included (can be used for range checking)
+        size_t stride; // distance between elements in sequence
+        friend std::ostream &operator<<(std::ostream &os, const slice &s)
+        {
+            return os << "slice(" << s.start << ", " << s.length << ", " << s.stride << ")";
+        }
     };
 
 }
