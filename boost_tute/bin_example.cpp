b5bd3a3010ea805f6498901983d2ef61b2e4a368
0xdomyz <yzdominic@outlook.com>
2023-04-29 19:29:22 +1000

    add boost bin example

---
+++ boost_tute/bin_example.cpp
@@ -0,0 +1,21 @@
+// g++ -I /usr/local/boost_1_82_0 -o bin_example bin_example.cpp /usr/local/lib/libboost_regex.a
+//
+// ./bin_example < email.txt
+
+#include <boost/regex.hpp>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string line;
+    boost::regex pat("^Subject: (Re: |Aw: )*(.*)");
+
+    while (std::cin)
+    {
+        std::getline(std::cin, line);
+        boost::smatch matches;
+        if (boost::regex_match(line, matches, pat))
+            std::cout << matches[2] << std::endl;
+    }
+}
