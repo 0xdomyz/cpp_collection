830ca8d103539539077b29ab7591c23b38da88c5
0xdomyz <yzdominic@outlook.com>
2023-05-06 20:22:14 +1000

    udpate sq int

--- accelerated/org/exercise/square_of_int.cpp
+++ accelerated/org/exercise/square_of_int.cpp
@@ -1,4 +1,5 @@
-// g++ square_of_int.cpp -o square_of_int && ./square_of_int
+// g++ square_of_int.cpp -o square_of_int && ./square_of_int 100
+// g++ square_of_int.cpp -o square_of_int && ./square_of_int 999
 
 #include <iostream>
 #include <vector>
@@ -6,12 +7,14 @@
 
 using namespace std;
 
-int main(void)
+// allow cmd args
+int main(int argc, char *argv[])
 {
-    vector<double> ints;
-    vector<double> squares;
+    vector<int> ints;
+    vector<int> squares;
 
-    const int max_int = 100;
+    const int max_int = argv[1] ? atoi(argv[1]) : 100;
+    cout << "max_int: " << max_int << endl;
 
     for (auto i = 0; i < max_int; i++)
     {
@@ -24,12 +27,19 @@ int main(void)
          << "squares" << endl;
 
     // display
-    for (auto i = 0; i < max_int; i += 10)
+    const string max_int_str = to_string(ints[max_int - 1]);
+    const auto max_int_width = max_int_str.size();
+    cout << "max_int_str: " << max_int_str << endl;
+
+    const string max_sqr_str = to_string(squares[max_int - 1]);
+    const auto max_width = max_sqr_str.size();
+    cout << "max_sqr_str: " << max_sqr_str << endl;
+
+    for (auto i = 0; i < max_int; i += max_int / 10)
     {
         // setw
-        auto ss = setw(4);
-        cout << ss << ints[i]
+        cout << setw(max_int_width) << ints[i]
              << "\t"
-             << ss << squares[i] << endl;
+             << setw(max_width) << squares[i] << endl;
     }
 }
