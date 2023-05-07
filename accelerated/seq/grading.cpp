b0accbc5bd7e672c5f0768f29bd810089e553469
0xdomyz <yzdominic@outlook.com>
2023-05-07 20:39:23 +1000

    run new functionality

---
+++ accelerated/seq/grading.cpp
@@ -0,0 +1,40 @@
+// compile: g++ -o grading grading.cpp Student_info.cpp grade.cpp median.cpp seperate.cpp && ./grading < grades.txt
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+#include "grade.h"
+#include "seperate.h"
+
+using std::cin;
+using std::cout;
+using std::sort;
+using std::vector;
+
+int main()
+{
+    vector<Student_info> students;
+
+    // read and store all the students data.
+    Student_info record;
+    while (read(cin, record))
+    {
+        students.push_back(record);
+    }
+
+    // alphabetize the student records
+    sort(students.begin(), students.end(), compare);
+
+    // extract the fails
+    vector<Student_info> fails = extract_fails(students);
+
+    // write the names and grades of not fails
+    cout << "Passing students:\n";
+    write_names_and_grade(students);
+
+    // write the names and grades of the fails
+    cout << "\nFailing students:\n";
+    write_names_and_grade(fails);
+
+    return 0;
+}
