0433d9a5da200104740b659f708e048f29e1372d
0xdomyz <yzdominic@outlook.com>
2023-05-08 11:36:53 +1000

    upate sepe

--- accelerated/seq/seperate.h
+++ accelerated/seq/seperate.h
@@ -4,8 +4,6 @@
 #include "Student_info.h"
 #include <vector>
 
-bool fgrade(const Student_info &s);
-
 // separate passing and failing student records: first try
 std::vector<Student_info> extract_fails(std::vector<Student_info> &students);
 
