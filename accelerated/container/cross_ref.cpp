6b4c7840f57e41adc82564e37494f04a31def244
0xdomyz <yzdominic@outlook.com>
2023-05-14 11:49:08 +1000

    add urls to prep for xref

--- accelerated/container/cross_ref.cpp
+++ accelerated/container/cross_ref.cpp
@@ -1,4 +1,4 @@
-// g++ split.cpp cross_ref.cpp -o cross_ref -std=c++20 && ./cross_ref
+// g++ urls.cpp split.cpp cross_ref.cpp -o cross_ref -std=c++20 && ./cross_ref
 
 #include <iostream>
 #include <string>
@@ -6,6 +6,7 @@
 #include <vector>
 #include <algorithm>
 #include "split.h"
+#include "urls.h"
 
 using namespace std;
 
@@ -14,6 +15,13 @@ int main(void)
     const string s = "This is a test string";
     vector<string> v = split(s);
     for (vector<string>::const_iterator i = v.begin(); i != v.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+
+    const string s2 = "This is a https://adsf test str://ing";
+    vector<string> v2 = find_urls(s2);
+    for (vector<string>::const_iterator i = v2.begin(); i != v2.end(); ++i)
     {
         cout << *i << endl;
     }
