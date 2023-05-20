57c45dadb7e34005e58bb5d5257270b579b81880
0xdomyz <yzdominic@outlook.com>
2023-05-20 19:55:59 +1000

    add letter_grades

--- accelerated/org/grading.cpp
+++ accelerated/org/grading.cpp
@@ -1,5 +1,4 @@
-// compile: g++ -o grading grading.cpp Student_info.cpp grade.cpp median.cpp
-// run: ./grading < grades.txt
+// compile: g++ -o grading letter_grades.cpp grading.cpp Student_info.cpp grade.cpp median.cpp && ./grading < grades.txt
 
 // organization
 // grade.cpp uses grade.h, median.h, Student_info.h
@@ -13,13 +12,16 @@
 #include <stdexcept>
 #include <string>
 #include <vector>
+#include <map>
 #include "grade.h"
+#include "letter_grades.h"
 // #include "Student_info.h" // not needed because grade.h includes it
 
 using std::cin;
 using std::cout;
 using std::domain_error;
 using std::endl;
+using std::map;
 using std::max;
 using std::setprecision;
 using std::sort;
@@ -41,9 +43,14 @@ int main()
         maxlen = max(maxlen, record.name.size());
         students.push_back(record);
     }
+
     // alphabetize the student records
     sort(students.begin(), students.end(), compare);
+
     // write the names and grades
+    // also put into right map
+    map<string, float> grade_to_count_map;
+
     for (vector<Student_info>::size_type i = 0;
          i != students.size(); ++i)
     {
@@ -54,6 +61,12 @@ int main()
         try
         {
             double final_grade = grade(students[i]);
+
+            // put into right map
+            string letter_grade = score_to_letter_grades(final_grade);
+            grade_to_count_map[letter_grade] += 1;
+
+            // display score
             streamsize prec = cout.precision();
             cout << setprecision(3) << final_grade
                  << setprecision(prec);
@@ -64,5 +77,13 @@ int main()
         }
         cout << endl;
     }
+
+    // display grade distribution
+    for (auto it = grade_to_count_map.begin();
+         it != grade_to_count_map.end(); ++it)
+    {
+        cout << it->first << ": " << it->second << endl;
+    }
+
     return 0;
 }
