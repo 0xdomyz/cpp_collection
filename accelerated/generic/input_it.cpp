0ff52bc983c69fb3c8d5dc7eb6124f9714544f72
0xdomyz <yzdominic@outlook.com>
2023-05-22 12:54:48 +1000

    udpte input

--- accelerated/generic/input_it.cpp
+++ accelerated/generic/input_it.cpp
@@ -7,12 +7,21 @@
 
 using namespace std;
 
+// template <class In, class X>
+// In find(In begin, In end, const X &x)
+// {
+//     while (begin != end && *begin != x)
+//         ++begin;
+//     return begin;
+// }
+
 template <class In, class X>
 In find(In begin, In end, const X &x)
 {
-    while (begin != end && *begin != x)
-        ++begin;
-    return begin;
+    if (begin == end || *begin == x)
+        return begin;
+    begin++;
+    return find(begin, end, x);
 }
 
 int main()
