3a48324fbd7dc4217bd30f571295f45b36303b88
0xdomyz <yzdominic@outlook.com>
2023-05-22 14:39:01 +1000

    update input it

--- accelerated/generic/input_it.cpp
+++ accelerated/generic/input_it.cpp
@@ -4,25 +4,9 @@
 #include <string>
 #include <vector>
 #include <list>
+#include "input_it.h"
 
 using namespace std;
-
-// template <class In, class X>
-// In find(In begin, In end, const X &x)
-// {
-//     while (begin != end && *begin != x)
-//         ++begin;
-//     return begin;
-// }
-
-template <class In, class X>
-In find(In begin, In end, const X &x)
-{
-    if (begin == end || *begin == x)
-        return begin;
-    begin++;
-    return find(begin, end, x);
-}
 
 int main()
 {
