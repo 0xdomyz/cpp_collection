ad9fe9c56bf9df503253045b3a3ddc724212c65b
0xdomyz <yzdominic@outlook.com>
2023-05-26 21:24:24 +1000

    udpate test stud indo

--- accelerated/new/test_Student_info.cpp
+++ accelerated/new/test_Student_info.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-int main(void)
+void test_basic()
 {
     // Student_info s;
     // s.read(cin);
@@ -15,4 +15,17 @@ int main(void)
 
     cout << s.valid() << endl;
     cout << s.grade() << endl;
+}
+
+void test_exception()
+{
+    Student_info s;
+    s.grade();
+    s.read(cin);
+}
+
+int main(void)
+{
+    // test_basic();
+    test_exception();
 }
