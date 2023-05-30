3e84a0ba596c1f3a3eb6c4f0a99a1e617745b4f7
0xdomyz <yzdominic@outlook.com>
2023-05-30 21:10:54 +1000

    udpate vec

--- accelerated/abstract_data_types/vec.h
+++ accelerated/abstract_data_types/vec.h
@@ -13,6 +13,7 @@ public:
     Vec() { create(); }
     explicit Vec(size_type n, const T &val = T()) { create(n, val); }
     Vec(const Vec &v) { create(v.begin(), v.end()); }
+    Vec &operator=(const Vec &);
 
     size_type size() const
     {
@@ -31,5 +32,19 @@ private:
     iterator data;
     iterator limit;
 };
+
+template <class T>
+Vec<T> &Vec<T>::operator=(const Vec &rhs)
+{
+    // check for self-assignment
+    if (&rhs != this)
+    {
+        // free the array in the left-hand side
+        uncreate();
+        // copy elements from the right-hand to the left-hand side
+        create(rhs.begin(), rhs.end());
+    }
+    return *this;
+}
 
 #endif // GUARD_VEC_H
