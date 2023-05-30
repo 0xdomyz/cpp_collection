8ac9f5e673032018d7ee273b4c1caddc81f97211
0xdomyz <yzdominic@outlook.com>
2023-05-30 12:13:07 +1000

    updte vec

--- accelerated/abstract_data_types/vec.h
+++ accelerated/abstract_data_types/vec.h
@@ -5,10 +5,31 @@ template <class T>
 class Vec
 {
 public:
-    // interface
+    typedef T *iterator;
+    typedef const T *const_iterator;
+    typedef size_t size_type;
+    typedef T value_type;
+
+    Vec() { create(); }
+    explicit Vec(size_type n, const T &val = T()) { create(n, val); }
+    Vec(const Vec &v) { create(v.begin(), v.end()); }
+
+    size_type size() const
+    {
+        return limit - data;
+    }
+
+    T &operator[](size_type i) { return data[i]; }
+    const T &operator[](size_type i) const { return data[i]; }
+
+    iterator begin() { return data; }
+    const_iterator begin() const { return data; }
+    iterator end() { return limit; }
+    const_iterator end() const { return limit; }
+
 private:
-    T *data;  // first element in the Vec
-    T *limit; // one past the last element in the Vec
+    iterator data;
+    iterator limit;
 };
 
 #endif // GUARD_VEC_H
