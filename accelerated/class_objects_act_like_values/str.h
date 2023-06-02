d1906e327c00d6f41135629b69f8ba392ab69d1b
0xdomyz <yzdominic@outlook.com>
2023-06-02 12:58:58 +1000

    updae str and test

--- accelerated/class_objects_act_like_values/str.h
+++ accelerated/class_objects_act_like_values/str.h
@@ -3,8 +3,12 @@
 
 class Str
 {
+    friend std::istream &operator>>(std::istream &, Str &);
+    friend std::ostream &operator<<(std::ostream &, const Str &);
+
 public:
     typedef Vec<char>::size_type size_type;
+
     // default constructor; create an empty Str
     Str() {}
     // create a Str containing n copies of c
@@ -21,16 +25,60 @@ public:
         std::copy(b, e, std::back_inserter(data));
     }
 
-    friend std::istream &operator>>(std::istream &, Str &);
-    friend std::ostream &operator<<(std::ostream &, const Str &);
+    size_type size() const { return data.size(); }
+
+    char &operator[](size_type i) { return data[i]; }
+    const char &operator[](size_type i) const
+    {
+        return data[i];
+    }
+
+    Str &operator+=(const Str &s)
+    {
+        std::copy(s.data.begin(), s.data.end(),
+                  std::back_inserter(data));
+        return *this;
+    }
 
 private:
     Vec<char> data;
 };
+
+Str operator+(const Str &, const Str &);
+
+// implementations
 
 std::ostream &operator<<(std::ostream &os, const Str &s)
 {
     for (auto i = s.data.begin(); i != s.data.end(); ++i)
         os << *i;
     return os;
+}
+
+std::istream &operator>>(std::istream &is, Str &s)
+{
+    // obliterate existing value(s)
+    s.data.clear();
+    // read and discard leading whitespace
+    char c;
+    while (is.get(c) && std::isspace(c))
+        ; // nothing to do, except testing the condition
+          // if still something to read, do so until next whitespace character
+    if (is)
+    {
+        do
+            s.data.push_back(c); // compile error!, data is private
+        while (is.get(c) && !std::isspace(c));
+        // if we read whitespace, then put it back on the stream
+        if (is)
+            is.unget();
+    }
+    return is;
+}
+
+Str operator+(const Str &s, const Str &t)
+{
+    Str r = s;
+    r += t;
+    return r;
 }
--- accelerated/class_objects_act_like_values/test_str.cpp
+++ accelerated/class_objects_act_like_values/test_str.cpp
@@ -9,4 +9,26 @@ int main(void)
 {
     Str s = "hello";
     cout << s << endl;
+
+    // indexing
+    cout << s[0] << endl;
+
+    {
+        // conversions
+        double d = 10; // convert 10 to double and use the converted value to initialize d
+        double d2;
+        d2 = 10; // convert 10 to double and assign the converted value to d
+
+        Str s("hello"); // construct s
+
+        Str t = "hello"; // initialize t
+        s = "hello";     // assign a new value to s
+    }
+
+    {
+        // concatenation of Str objects and C-style strings
+        Str s = "hello";
+        Str t = "1" + s + " abc";
+        cout << t << endl;
+    }
 }
