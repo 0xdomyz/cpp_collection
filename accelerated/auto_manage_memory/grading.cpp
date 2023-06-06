3ecee7b383a2210db6c037f44c17868008ebf529
0xdomyz <yzdominic@outlook.com>
2023-06-06 16:46:30 +1000

    add grading

--- accelerated/auto_manage_memory/grading.cpp
+++ accelerated/auto_manage_memory/grading.cpp
@@ -1,4 +1,4 @@
-// g++ handle.cpp students.cpp grading.cpp -o grading && ./grading
+// g++ students.cpp grading.cpp -o grading && ./grading < mix.csv
 
 #include <vector>
 #include <iostream>
@@ -11,4 +11,36 @@
 
 using namespace std;
 
-int main(void) {}
+int main()
+{
+    vector<Student_info> students;
+    Student_info record;
+    string::size_type maxlen = 0;
+    // read and store the data
+    while (record.read(cin))
+    {
+        maxlen = max(maxlen, record.name().size());
+        students.push_back(record);
+    }
+    // alphabetize the student records
+    sort(students.begin(), students.end(), Student_info::compare);
+    // write the names and grades
+    for (vector<Student_info>::size_type i = 0;
+         i != students.size(); ++i)
+    {
+        cout << students[i].name()
+             << string(maxlen + 1 - students[i].name().size(), ' ');
+        try
+        {
+            double final_grade = students[i].grade();
+            streamsize prec = cout.precision();
+            cout << setprecision(3) << final_grade
+                 << setprecision(prec) << endl;
+        }
+        catch (domain_error e)
+        {
+            cout << e.what() << endl;
+        }
+    }
+    return 0;
+}
