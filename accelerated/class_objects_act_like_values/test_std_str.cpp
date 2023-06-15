7ccba1bb24ca3dd3c6d0306ed9fa51f3fcad8511
0xdomyz <yzdominic@outlook.com>
2023-06-16 02:57:54 +1000

    add test code

---
+++ accelerated/class_objects_act_like_values/test_std_str.cpp
@@ -0,0 +1,12 @@
+// g++ test_std_str.cpp -o test_std_str && ./test_std_str
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string s = "hello";
+    cout << s << endl;
+}
