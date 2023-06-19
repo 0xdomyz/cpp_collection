ad2dc5909f8c9ced2905ea6c5b9c549b8e6a61f7
0xdomyz <yzdominic@outlook.com>
2023-06-19 22:20:10 +1000

    update split

--- accelerated/class_objects_act_like_values/split/split.h
+++ accelerated/class_objects_act_like_values/split/split.h
@@ -5,9 +5,6 @@
 #include <string>
 #include <cctype>
 
-using std::find_if;
-using std::string;
-
 bool space(char c)
 {
     return isspace(c);
@@ -19,38 +16,38 @@ bool not_space(char c)
 }
 
 template <class Out> // changed
-void split(const string &str, Out os)
+void split(const std::string &str, Out os)
 { // changed
-    typedef string::const_iterator iter;
+    typedef std::string::const_iterator iter;
     iter i = str.begin();
     while (i != str.end())
     {
         // ignore leading blanks
-        i = find_if(i, str.end(), not_space);
+        i = std::find_if(i, str.end(), not_space);
         // find end of next word
-        iter j = find_if(i, str.end(), space);
+        iter j = std::find_if(i, str.end(), space);
         // copy the characters in [i, j)
         if (i != str.end())
-            *os++ = string(i, j); // changed
+            *os++ = std::string(i, j); // changed
         i = j;
     }
 }
 
 template <class container>
-container split(const string &str)
+container split(const std::string &str)
 {
-    typedef string::const_iterator iter;
+    typedef std::string::const_iterator iter;
     iter i = str.begin();
     container cont;
     while (i != str.end())
     {
         // ignore leading blanks
-        i = find_if(i, str.end(), not_space);
+        i = std::find_if(i, str.end(), not_space);
         // find end of next word
-        iter j = find_if(i, str.end(), space);
+        iter j = std::find_if(i, str.end(), space);
         // copy the characters in [i, j)
         if (i != str.end())
-            cont.push_back(string(i, j));
+            cont.push_back(std::string(i, j));
         i = j;
     }
     return cont;
